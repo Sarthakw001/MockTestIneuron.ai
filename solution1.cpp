3532f1e933d64afa4e41928f0e57ae282c41076f
Sarthakw001 <sarthakw01@gmail.com>
2023-06-04 15:09:49 +0530

    Solution Submission

---
+++ solution1.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void moveZeroes(vector<int> &nums)
+{
+    int k = 0;
+    for (int i = 0; i < nums.size(); i++)
+    {
+        if (nums[i] != 0)
+            nums[k++] = nums[i];
+    }
+    for (int i = k; i < nums.size(); i++)
+        nums[i] = 0;
+}
---
+++ solution2.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int firstNonRepeatingCharacter(string s)
+{
+    unordered_map<char, int> mp;
+    for (int i = 0; i < s.length(); i++)
+        mp[s[i]]++;
+
+    for (int i = 0; i < s.length(); i++)
+    {
+        if (mp[s[i]] == 1)
+            return i;
+    }
+    return -1;
+}
